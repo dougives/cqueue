b09d5019090e78493a5b24af7a103df951dfd8f2
dougives <doug.ives@gmail.com>
2016-06-02 10:53:32 -0500

    enq and deq

--- cqueue.c/cqueue.c/atomic.h
+++ cqueue.c/cqueue.c/atomic.h
@@ -3,3 +3,5 @@
 #include <stdint.h>
 
 extern bool atomic_lock(uint64_t* lock);
+extern void atomic_unlock(uint64_t* lock);
+extern bool atomic_set(uint64_t* addr, uint64_t value);
--- cqueue.c/cqueue.c/cqueue.c
+++ cqueue.c/cqueue.c/cqueue.c
@@ -5,43 +5,71 @@
 #include <immintrin.h>
 #include "atomic.h"
 
-typedef uint64_t CQMsgType;
-#define MSGTYPE_ALL		~0uL
-#define MSGTYPE_NONE	 0uL
+#define MSGTYPE_ALL		~0u
+#define MSGTYPE_NONE	 0u
 
-//typedef volatile uint32_t CQEntryState;
-//#define QES_FREE	0x00000000u
-//#define QES_ENQ		0x00000001u
-//#define QES_READY	0x00000002u
-//#define QES_DEQ		0x00000004u
-//#define QES_DIRTY	0x00000008u
+#define STATE_FREE	0x00000000u
+#define STATE_ENQ	0x00000001u
+#define STATE_READY	0x00000002u
+#define STATE_DEQ	0x00000004u
+//#define STATE_DIRTY	0x00000008u
 
 #define QSIZE		0x00000100u
 #define QMASK		(QSIZE - 1)
 #define MSGSIZE		0x00000100u
+#define HEADERSIZE	sizeof(struct CQMessageHeader)
 
 #define cq_isempty() (head == tail)
 #define cq_isfull() (head == (tail - 1))
 
 typedef union
 {
-	struct CQMessageHeader
+	struct CQMessageData
 	{
-		uint64_t lock;
-		uint64_t msgtype;
+		struct CQMessageHeader
+		{
+			volatile uint64_t lock;
+			volatile uint32_t state;
+			uint32_t type;
+		};
+		union CQMessagePayload
+		{
+			uint8_t payload[MSGSIZE - HEADERSIZE];
+		};
 	};
 	uint8_t data[MSGSIZE];
 } CQMessage;
 
 static CQMessage queue[QSIZE];
-static uint32_t lead = 0;
+
+CQMessage cq_deq()
+{
+	static uint32_t lead = 0;
+	CQMessage msg;
+	while (queue[++lead & QMASK].state != STATE_READY 
+		|| !atomic_lock(queue[lead].lock)) ;
+	while (!atomic_set(&queue[lead].state, STATE_DEQ)) ;
+	memcpy(&msg, &queue[lead], sizeof(CQMessage));
+	// wish i could return here ... maybe gc?
+	memcpy(queue[lead].data + HEADERSIZE, 0, sizeof(CQMessage));
+	queue[lead].type = MSGTYPE_NONE;
+	atomic_unlock(queue[lead].lock);
+	while (!atomic_set(&queue[lead].state, STATE_FREE));
+	return msg;
+}
 
 void cq_enq(CQMessage msg)
 {
-	while (atomic_lock(queue[(++lead & QMASK)].lock))
-	{
+	static uint32_t lead = 0;
 
-	}
+	while (queue[++lead & QMASK].state != STATE_FREE //0
+		|| !atomic_lock(queue[lead].lock))
+	while (!atomic_set(&queue[lead].state, STATE_ENQ))
+	//queue[lead].state = STATE_ENQ;
+	memcpy(&queue[lead], &msg, sizeof(CQMessage));
+	atomic_unlock(queue[lead].lock);
+	while (!atomic_set(&queue[lead].state, STATE_READY));
+	//queue[lead].state = STATE_READY;
 }
 
 int main(void)
