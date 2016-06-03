c8f9ba63e4894b999d791f51f3a826ea82023449
dougives <doug.ives@gmail.com>
2016-06-03 10:00:05 -0500

    can just lock pointers

--- cqueue/cqueue/cqueue.c
+++ cqueue/cqueue/cqueue.c
@@ -5,39 +5,39 @@
 #include <windows.h>
 #include "atomic.h"
 
-#define STATE_FREE	0x00000000u
-#define STATE_ENQ	0x00000001u
-#define STATE_READY	0x00000002u
-
 #define DATA_SIZE 0x100
 #define QBUFFER_SIZE 0x100
 #define QBUFFER_MASK (QBUFFER_SIZE - 1)
 
 typedef struct
 {
-	uint32_t state;
 	uint8_t data[DATA_SIZE];
 } Message;
 
+
 volatile static Message* qbuffer[QBUFFER_SIZE];
 
+bool cq_deq(Message* msg)
+{
+	volatile static size_t index = 0;
+	size_t i = index & QBUFFER_MASK;
+	for (i; i = ++index & QBUFFER_MASK; i++)
+	{
+
+	}
+
+}
 
 void cq_enq(Message* msg)
 {
 	volatile static size_t index = 0;  // yeehaw
-	msg->state = STATE_ENQ;
 	size_t i = index & QBUFFER_MASK;
 	for (i; i = ++index & QBUFFER_MASK; i++)
 	{
-		if (qbuffer[i]->state == STATE_FREE
-			&& atomic_set32(&qbuffer[i]->state, STATE_ENQ)) // could do bitwise atomics
+		if (qbuffer[i] == NULL
+			&& atomic_set64(&qbuffer[i], msg))
 			break;
 	}
-
-	qbuffer[i] = msg;
-
-	while(!atomic_set32(&qbuffer[i]->state, STATE_READY))
-	{ }
 	return;
 }
 
