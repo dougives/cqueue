a9ff2dc0785f9c0fe0cc853237ec87f8106ff12c
dougives <doug.ives@gmail.com>
2016-06-03 11:41:53 -0500

    gonna use something other than cas

--- cqueue/cqueue/cqueue.c
+++ cqueue/cqueue/cqueue.c
@@ -6,7 +6,7 @@
 #include "atomic.h"
 
 #define DATA_SIZE 0x100
-#define QBUFFER_SIZE 0x100
+#define QBUFFER_SIZE 0x40
 #define QBUFFER_MASK (QBUFFER_SIZE - 1)
 
 typedef struct
@@ -21,9 +21,9 @@ volatile static Message* qbuffer[QBUFFER_SIZE];
 Message* cq_deq()
 {
 	volatile static size_t index = 0;
-	Message* msg;
-	size_t i = index & QBUFFER_MASK;
-	for (i; i = ++index & QBUFFER_MASK; i++)
+	Message* msg = NULL;
+	size_t i = ++index & QBUFFER_MASK;
+	for (i; i = index & QBUFFER_MASK; i++)
 	{
 		if (qbuffer[i] != NULL 
 			&& qbuffer[i]->lock == ATOMIC_UNLOCKED
@@ -43,7 +43,7 @@ void cq_enq(Message* msg)
 	volatile static size_t index = 0;  // yeehaw
 
 	msg->lock = ATOMIC_UNLOCKED;
-	for (size_t i = index & QBUFFER_MASK; i = ++index & QBUFFER_MASK; i++)
+	for (size_t i = ++index & QBUFFER_MASK; i = index & QBUFFER_MASK; i++)
 	{
 		if (qbuffer[i] == NULL
 			&& atomic_set64((uint64_t*)&qbuffer[i], (uint64_t)msg))
@@ -52,13 +52,60 @@ void cq_enq(Message* msg)
 	return;
 }
 
+volatile static long tp = 0;
+void test_enq()
+{
+	while (true)
+	{
+		Message msg;
+		Message* msgptr = &msg;
+		memset(msgptr, 0, sizeof(Message));
+		cq_enq(msgptr);
+		//Sleep(1);
+	}
+}
+
+void test_deq()
+{
+	Message* msgptr = NULL;
+	while (true)
+	{
+		msgptr = cq_deq();
+		tp++;
+		//Sleep(1);
+	}
+}
+
+void test_tp()
+{
+	while (true)
+	{
+		tp = 0;
+		Sleep(1000);
+		printf("%d\n", tp);
+	}
+}
+
 
 int main(void)
 {
 	Message msg;
-	memset(&msg, 0, sizeof(Message));
 	Message* msgptr = &msg;
+	memset(msgptr, 0, sizeof(Message));
 	cq_enq(msgptr);
 	msgptr = cq_deq();
+
+	LPDWORD threadid = NULL;
+	CreateThread(NULL, 0, test_tp, NULL, 0, threadid);
+	CreateThread(NULL, 0, test_deq, NULL, 0, threadid);
+	CreateThread(NULL, 0, test_enq, NULL, 0, threadid);	
+	CreateThread(NULL, 0, test_deq, NULL, 0, threadid);
+	CreateThread(NULL, 0, test_enq, NULL, 0, threadid);	
+	CreateThread(NULL, 0, test_deq, NULL, 0, threadid);
+	CreateThread(NULL, 0, test_enq, NULL, 0, threadid);
+
+	while (true)
+		Sleep(100);
+
 	return 0;
 }
